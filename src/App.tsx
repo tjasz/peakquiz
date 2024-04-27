7feab07841deaa5e8a1ef5247fce49e3e7a10f47
tjasz <nukorausa@hotmail.com>
2024-04-26 17:19:19 -0700

    Track a list of guesses

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import './App.css';
 function App() {
   const id = useId();
   const [draft, setDraft] = useState<null|string>(null);
-  const [currentGuess, setCurrentGuess] = useState<null|string>(null);
+  const [guesses, setGuesses] = useState<string[]>([]);
 
   const handleInput : React.FormEventHandler<HTMLInputElement> = (ev) => {
     setDraft(ev.currentTarget.value);
   };
   const handleSubmit : React.FormEventHandler<HTMLFormElement> = (ev) => {
     ev.preventDefault();
-    setCurrentGuess(draft);
+    if (draft && guesses.indexOf(draft) < 0) {
+      setGuesses([...guesses, draft]);
+    }
     setDraft(null);
   };
 
@@ -25,7 +27,9 @@ function App() {
         <input type="text" id={id} value={draft ?? ""} onInput={handleInput} />
         <input type="submit" value="Submit" />
       </form>
-      <p>{currentGuess}</p>
+      <ul>
+        {guesses.map(guess => (<li>{guess}</li>))}
+      </ul>
     </div>
   );
 }
