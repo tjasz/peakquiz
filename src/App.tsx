4469dea14b90e02ce6b7f147397dc57a346f4e69
tjasz <nukorausa@hotmail.com>
2024-04-26 17:12:45 -0700

    Track guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useId, useState } from 'react';
 import './App.css';
 
 function App() {
+  const id = useId();
+  const [draft, setDraft] = useState<null|string>(null);
+  const [currentGuess, setCurrentGuess] = useState<null|string>(null);
+
+  const handleInput : React.FormEventHandler<HTMLInputElement> = (ev) => {
+    setDraft(ev.currentTarget.value);
+  };
+  const handleSubmit : React.FormEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
+    setCurrentGuess(draft);
+    setDraft(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>PeakQuiz</p>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={id}>Guess:</label>
+        <input type="text" id={id} value={draft ?? ""} onInput={handleInput} />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>{currentGuess}</p>
     </div>
   );
 }
